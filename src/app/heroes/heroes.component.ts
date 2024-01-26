4caafe9f7f6617d1facf561632ee59088400e922
Samson <samson.asefa@tobania.be>
2024-01-26 15:10:16 +0300

    created hero service and inject to HeroComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Hero } from '../Hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +9,21 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
